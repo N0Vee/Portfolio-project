400fb7b526fe9d0f7568938910935a43fceba081
N0Vee <wanidchanon@gmail.com>
2025-02-07 16:52:49 +0700

    Commit: SkillSection use factory pattern to design component

--- src/components/SkillSection.jsx
+++ src/components/SkillSection.jsx
@@ -1,77 +1,66 @@
-import React from 'react'
+import React from 'react';
 
 export const SkillSection = () => {
-
-    const frontend = [
-        "html5", "css3", "js", "react"
-    ];
-
-    const ui = [
-        "Bulma", "bootstrap", "Tailwind"
-    ];
-
-    const backend= [
-        "node-js", "Express"
-    ];
-
-    const database= [
-        "MySQL", "PostgreSQL", "MongoDB", "Firebase"
-    ];
+    const frontend = ["html5", "css3", "js", "react"];
+    const ui = ["Bulma", "bootstrap", "Tailwind"];
+    const backend = ["node-js", "Express"];
+    const database = ["MySQL", "PostgreSQL", "MongoDB", "Firebase"];
 
     const iconMap = {
-        "Bulma" : "https://bulma.io/assets/brand/Bulma%20Logo%20White.png",
+        "Bulma": "https://bulma.io/assets/brand/Bulma%20Logo%20White.png",
         "Tailwind": "https://www.loopple.com/img/tailwind-logo.png",
-        "Express" : "https://img.icons8.com/?size=100&id=kg46nzoJrmTR&format=png&color=abb1bf",
-        "MySQL" : "MySQL",
-        "PostgreSQL" : "PostgreSQL",
-        "MongoDB" : "MongoDB",
-        "Firebase" : "Firebase"
-        
+        "Express": "https://img.icons8.com/?size=100&id=kg46nzoJrmTR&format=png&color=abb1bf",
+        "MySQL": "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        "PostgreSQL": "https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg",
+        "MongoDB": "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+        "Firebase": "https://raw.githubusercontent.com/devicons/devicon/master/icons/firebase/firebase-original-wordmark.svg"
     };
 
-    const SkillSection = (title, type) => {
-        return(
+    
+    const SkillSectionFactory = (title, type) => {
+        return () => (
             <>
                 <p className='mt-4'>{title}</p>
-                        <div className="grid">
-                            <div className="columns is-multiline">
-                                {type.map((iconName, index) => (
-                                    <div key={index} className="column is-one-four">
-                                        <div className="is-center mx-5 my-5">
-                                            <span className="icon">
-                                                {iconMap[iconName] ? (
-                                                    <img id='imgIcon' src={iconMap[iconName]} alt={iconName} className=''/>
-                                                ) : (
-                                                    <i id='Icon' className={'fab fa-' + iconName}></i>
-                                                )}
-                                            </span>
-                                        </div>
-                                    </div>
-                                ))}
+                <div className="grid">
+                    <div className="columns is-multiline">
+                        {type.map((iconName, index) => (
+                            <div key={index} className="column is-one-four">
+                                <div className="is-center mx-5 my-5">
+                                    <span className="icon">
+                                        {iconMap[iconName] ? (
+                                            <img id='imgIcon' src={iconMap[iconName]} alt={iconName} className='' />
+                                        ) : (
+                                            <i id='Icon' className={'fab fa-' + iconName}></i>
+                                        )}
+                                    </span>
+                                </div>
                             </div>
-                        </div>
-            </>
-        )
-    };
-
-    return (
-        <>
-            <div className="container">
-                <div className="content">
-                    <div className="is-flex is-justify-content-center is-flex-direction-column is-align-items-center">
-                        
-                        <h3 className=''>Featured Skill</h3>
-                        
-                        {SkillSection("Frontend/Framework", frontend)}
-                        {SkillSection("UI Libraries", ui)}
-                        {SkillSection("Backend", backend)}
-                        {SkillSection("Databases", database)}
-
+                        ))}
                     </div>
                 </div>
+            </>
+        );
+    };
+
+    
+    const FrontendSection = SkillSectionFactory("Frontend/Framework", frontend);
+    const UISection = SkillSectionFactory("UI Libraries", ui);
+    const BackendSection = SkillSectionFactory("Backend", backend);
+    const DatabaseSection = SkillSectionFactory("Databases", database);
+
+    return (
+        <div className="container">
+            <div className="content">
+                <div className="is-flex is-justify-content-center is-flex-direction-column is-align-items-center">
+                    <h3 className=''>Featured Skill</h3>
+                    <FrontendSection />
+                    <UISection />
+                    <BackendSection />
+                    <DatabaseSection />
+                </div>
             </div>
-        </>
-    )
-}
+        </div>
+    );
+};
 
 export default SkillSection;

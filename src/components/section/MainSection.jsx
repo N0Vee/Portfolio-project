17f8533ade5f1db5c62df1230ae425e4861416d9
N0Vee <wanidchanon@gmail.com>
2025-03-20 21:01:44 +0700

    Commit: Fix Bug

--- src/components/section/MainSection.jsx
+++ src/components/section/MainSection.jsx
@@ -66,9 +66,7 @@ const MainSection = () => {
                                     <i className="fas fa-envelope"></i>
                                 </a>
                                 
-                                <button onClick={window.open('images/Wanichanon_SaeLee_Resume.pdf')} className="button about-button has-text-white">
-                                    <span>Resume</span>
-                                </button>
+                                <button onClick={() => window.open('images/Wanichanon_SaeLee_Resume.pdf')} className="button about-button has-text-white"><span>Resume</span></button>
                             </div>
                         </div>
                         
